b69eac6ab33fde7ee631197c5e3080d50b3e2717
Mohamed_Hossam <mohamedhossam25709@gmail.com>
2025-03-18 01:35:18 +0200

    FIRST COMMIT OF THE MEMZY APP

--- Models/MemzyContext.cs
+++ Models/MemzyContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-
+using static System.Console;
 namespace Memzy_finalist.Models
 {
     public partial class MemzyContext : DbContext
--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using static System.Console;
 namespace MyProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
