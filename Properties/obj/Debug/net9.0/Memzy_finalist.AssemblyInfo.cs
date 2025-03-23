9d967751fd280498da1e1cf78e2d4cf0a2644c7c
Mohamed_Hossam <mohamedhossam25709@gmail.com>
2025-03-23 15:17:49 +0200

    I am trying to pull a working version...

--- Properties/obj/Debug/net9.0/Memzy_finalist.AssemblyInfo.cs
+++ Properties/obj/Debug/net9.0/Memzy_finalist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5b16d1682af597d65e31cff400a819d5ceac49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784d866602c951f8edc60eba786cc99f6373f9c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net9.0/Memzy_finalist.AssemblyInfo.cs
+++ obj/Debug/net9.0/Memzy_finalist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4992de6ba79a90db36d506e7e51244041c3900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784d866602c951f8edc60eba786cc99f6373f9c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Memzy_finalist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
