1b8e920a7fd29b2fdd411fb6fef269a9de0006f8
Mohamed_Hossam <mohamedhossam25709@gmail.com>
2025-05-24 19:44:30 +0300

    More cleaning for the backend

--- Backend/Controllers/MessagingController.cs
+++ Backend/Controllers/MessagingController.cs
@@ -111,16 +111,4 @@ namespace MyApiProject.Controllers
             }
         }
     }
-
-    public class MessageDto
-    {
-        [Required(ErrorMessage = "Receiver ID is required")]
-        [Range(1, int.MaxValue, ErrorMessage = "Invalid receiver ID")]
-        public int ReceiverId { get; set; }
-
-        [Required(ErrorMessage = "Message content is required")]
-        [StringLength(1000, MinimumLength = 1, 
-            ErrorMessage = "Message must be between 1 and 1000 characters")]
-        public string Content { get; set; }
-    }
 }
